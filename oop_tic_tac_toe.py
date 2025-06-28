a15018fd48cb8a92238d02926f19719cde8dae92
Carlos <carlosroque117@gmail.com>
2025-06-28 17:49:58 +0800

    Added window format then a code to run the to show the window.

--- oop_tic_tac_toe.py
+++ oop_tic_tac_toe.py
@@ -48,3 +48,8 @@ class TicTacToe:
 
         window_x = int((screen_width/2) - (window_width/2))
         window_y = int((screen_height/2) - (window_height/2))
+
+        #format "(w)x(h)+(x)+(y)"
+        self.window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
+
+        self.window.mainloop()
